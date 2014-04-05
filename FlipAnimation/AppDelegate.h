7f6f8b723beacf660a0b7314d5653c15f0b5dfa0
Rounak Jain <rounak91@gmail.com>
2014-04-06 04:04:08 +0530

    Initial Commit

---
+++ FlipAnimation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlipAnimation
+//
+//  Created by Rounak Jain on 06/04/14.
+//  Copyright (c) 2014 Rounak Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
